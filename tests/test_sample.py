42f8e669529a4f854f3bc87059fcd25bfdf99fc1
keris <dlq137@gmail.com>
2019-11-13 15:10:34 +0800

    Initial commit

---
+++ tests/test_sample.py
@@ -0,0 +1,9 @@
+# This script serves to be run as a test to make sure circleci setup is OK
+from __future__ import print_function, absolute_import, division
+
+def incr(x):
+    return x + 1
+
+
+def test_itworks():
+    assert incr(1) == 3
